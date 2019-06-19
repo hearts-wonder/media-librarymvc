ec6e315376b2e2e71f75e82c9d0d2d71565c6486
cellia <cellia.m@live.fr>
2019-06-19 14:26:47 +0200

    install bramus router + examples

--- index.php
+++ index.php
@@ -1,44 +1,24 @@
-   <?php
+<?php
+use Bramus\Router\Router;
+
+require __DIR__ . '/vendor/autoload.php';
     
-    require __DIR__ . '/vendor/autoload.php';
-    
-    $new = [
-        ' article 1',
-        'article 2',
-        'article 3'
-    ];
+$router = new Router;
 
-    var_dump($new);
-dump($new);
+$router ->get('/hello', function(){
+    echo "Hello World";
+});
 
-
+$router->get('/hello/{num}', function($num) {
+    echo "page n°" . $num;
+});
+$router->run();
 
 
 
-    ?>
+
    
    
    
-   <!doctype html>
-   <html lang="en">
-
-   <head>
-       <title>Title</title>
-       <!-- Required meta tags -->
-       <meta charset="utf-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-       <!-- Bootstrap CSS -->
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   </head>
-
-   <body>
-
-       <!-- Optional JavaScript -->
-       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   </body>
-
-   </html>
+   
+   
